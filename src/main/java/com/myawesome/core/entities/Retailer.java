a8ef3f12ff2157afa2a553c898a9cf77d79da6b1
Prakash Shanbhag <praks.1529@gmail.com>
2020-07-12 21:55:08 +0530

    Cleanup pom.xml

--- src/main/java/com/myawesome/core/entities/Retailer.java
+++ src/main/java/com/myawesome/core/entities/Retailer.java
@@ -3,7 +3,6 @@ package com.myawesome.core.entities;
 import com.myawesome.core.constants.enums.ProfileType;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
-import org.neo4j.ogm.annotation.Property;
 
 import javax.persistence.*;
 
@@ -21,22 +20,13 @@ public class Retailer {
     String id;
     @Enumerated(EnumType.STRING)
     ProfileType profileType;
-    @Property
     String name;
-    @Property
     String storeName;
-    @Property
     Double latitude;
-    @Property
     Double longitude;
-    @Property
     String email;
-    @Property
     String phoneNumber;
-    @Property
     String gstin;
-    @Property
     String addressLine1;
-    @Property
     Boolean verified;
 }
